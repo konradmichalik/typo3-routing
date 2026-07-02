29010cd9fce321b2654eb7c895677d2b269c92a9
Konrad Michalik <km@move-elevator.de>
2026-07-02 22:02:41 +0200

    style: add missing file header to fixture TCA file

--- Tests/Functional/Fixtures/Extensions/routing_test/Configuration/TCA/tx_routingtest_domain_model_item.php
+++ Tests/Functional/Fixtures/Extensions/routing_test/Configuration/TCA/tx_routingtest_domain_model_item.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the "typo3_routing" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return [
     'ctrl' => [
         'title' => 'Item',
